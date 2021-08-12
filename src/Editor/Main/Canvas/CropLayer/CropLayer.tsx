fc57081df54c8aa80dba2c5dc8a1f8c1eeb0f1e7
XuYuCheng <xuyucheng@tezign.com>
2021-08-12 20:32:40 +0800

    fix(crop): quit crop will not update crop data correctly

--- src/Editor/Main/Canvas/CropLayer/CropLayer.tsx
+++ src/Editor/Main/Canvas/CropLayer/CropLayer.tsx
@@ -1,7 +1,5 @@
 import { cropData } from "@/core/data/cropData";
-import { rafBatch, useValue } from "@/core/utils";
-import { updateCurrentImageCropInfo } from "@/logic/action/currentImage";
-import { getCropData } from "@/logic/get/cropData";
+import { useValue } from "@/core/utils";
 import { getCurrentImage } from "@/logic/get/currentImage";
 import type { CSSProperties, FC } from "react";
 import { memo, useEffect, useMemo } from "react";
@@ -11,24 +9,11 @@ import { Cropper } from "./Cropper/Cropper";
 
 export const CropLayer: FC<{ style?: CSSProperties }> = memo(({ style }) => {
   const currImage = useValue(getCurrentImage);
-  const cropStore = useValue(getCropData);
 
-  // init when mount
   useEffect(() => {
-    rafBatch(() => {
-      const { width: originWidth, height: originHeight } = currImage.origin;
-      cropData.set({ ...currImage.crop, originWidth, originHeight });
-    }).then();
-    // eslint-disable-next-line
-  }, []);
-
-  // sync when unmount
-  useEffect(() => {
-    return () => {
-      const { width, height, x, y, flip, rotate } = cropStore;
-      updateCurrentImageCropInfo({ width, height, x, y, flip, rotate });
-    };
-  }, [cropStore]);
+    const { width: originWidth, height: originHeight } = currImage.origin;
+    cropData.set({ ...currImage.crop, originWidth, originHeight });
+  }, [currImage]);
 
   return useMemo(() => <Cropper />, [style]);
 });
