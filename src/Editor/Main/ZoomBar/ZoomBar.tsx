c815ed76f08e2fe3cc632bd30e86facd7b373348
XuYuCheng <xuyucheng@tezign.com>
2021-08-17 11:51:25 +0800

    refactor(zoom-bar-style): opt zoomBar style

--- src/Editor/Main/ZoomBar/ZoomBar.tsx
+++ src/Editor/Main/ZoomBar/ZoomBar.tsx
@@ -1,8 +1,9 @@
-import { css } from "@emotion/react";
+import { css, cx } from "@emotion/css";
 import type { FC } from "react";
 import { memo } from "react";
 import { Dropdown, Icon } from "tezign-ui";
-import tw from "twin.macro";
+// @ts-ignore
+import { tw } from "twind";
 import { getFormatScale, scaleDown, scaleUp, useScale } from "../logic/scale";
 import { ZoomList } from "./ZoomList";
 
@@ -12,9 +13,9 @@ export const ZoomBar: FC = memo(() => {
   const scaleValue = useScale();
 
   return (
-    <div css={[tw`absolute bottom-10 left-1/2 z-50 flex justify-between`, zoomBarContainerStyle]}>
-      <div css={[tw`h-9 px-3 flex items-center justify-between`, zoomBarStyle]}>
-        <div css={[tw`p-1 flex items-center mr-2 cursor-pointer`, iconJustifyStyle]} onClick={scaleDown}>
+    <div className={cx(tw`absolute bottom-10 left-1/2 z-50 flex justify-between`, container)}>
+      <div className={cx(tw`h-9 px-3 flex items-center justify-between`, zoomBar)}>
+        <div className={cx(tw`p-1 flex items-center mr-2 cursor-pointer`, icon)} onClick={scaleDown}>
           <Icon type="stop" className="fz-16" />
         </div>
 
@@ -22,12 +23,12 @@ export const ZoomBar: FC = memo(() => {
           overlay={<ZoomList />}
           trigger={["click"]}
           placement="topCenter"
-          css={tw`cursor-pointer h-full flex items-center select-none`}
+          className={tw`cursor-pointer h-full flex items-center select-none`}
         >
           <span>{getFormatScale(scaleValue)}</span>
         </Dropdown>
 
-        <div css={tw`p-1 flex items-center ml-2 cursor-pointer`} onClick={scaleUp}>
+        <div className={tw`p-1 flex items-center ml-2 cursor-pointer`} onClick={scaleUp}>
           <Icon type="add-plus" className="fz-16" />
         </div>
       </div>
@@ -37,17 +38,17 @@ export const ZoomBar: FC = memo(() => {
 
 // * --------------------------------------------------------------------------- style
 
-const zoomBarContainerStyle = css`
+const container = css`
   transform: translate(-50%);
 `;
 
-const zoomBarStyle = css`
+const zoomBar = css`
   border-radius: 40px;
   background-color: #2f2f2f;
 `;
 
 // 组件库的 +/- icon 有 1px 的误差，通过 padding 调整一下，保持水平居中
-const iconJustifyStyle = css`
+const icon = css`
   padding-top: 5px;
   padding-bottom: 3px;
 `;
